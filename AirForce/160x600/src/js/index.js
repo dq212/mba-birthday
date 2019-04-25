11e58c80acc81067b8a1f0d580f3748494660ed2
o2kl <dquintero@o2kl.com>
2019-04-25 15:22:59 -0400

    second commit

---
+++ AirForce/160x600/src/js/index.js
@@ -0,0 +1,97 @@
+var nameSpace = MBA || {};
+
+( function () {
+	"use strict";
+	
+	var timeline;
+	var wrapper, click_through, logo, copy, cta, width, height;
+	
+	nameSpace.init = function () {
+		// Initialize any variables here
+		wrapper = nameSpace.$( '#wrapper' );
+		click_through = document.getElementById('click_through');
+
+		width = 160;
+		height = 600;
+
+		wrapper.addClass( 'show' );
+
+		nameSpace.initClickTag();
+		nameSpace.initAnimation();
+
+		if ( nameSpace.useFallback() ) {
+			nameSpace.injectFallback();
+		} else {
+		nameSpace.startAnimation();
+		}
+
+
+		click_through.onmouseover = function () {	
+		    TweenMax.to("#arrow", 0.2, { x:2, transformOrigin:"75% 60%", z:0.01, force3D:true, rotationZ: 0.01, transformPerspective: 400});
+		};
+
+		click_through.onmouseout = function () {
+		    TweenMax.to("#arrow", 0.2, {scale:1, force3D:true, z:0.01, rotationZ: 0.01, transformPerspective: 400, x:0});
+		};
+	};
+
+	nameSpace.initClickTag = function () {
+		click_through.onclick = function () {
+			window.open( window.clickTag );
+		};		
+	};
+
+	nameSpace.injectFallback = function() {
+		var body = document.body;
+
+		while ( body.firstChild ) {
+			body.removeChild( body.firstChild );
+		}
+
+		var anchor = document.createElement('a');
+		anchor.style.cursor = 'pointer';
+
+		var img = new Image();
+			img.src = './img/static.jpg';
+
+		anchor.appendChild( img );
+		anchor.onclick = function() { window.open(window.clickTag); };
+		document.body.appendChild( anchor );
+	};
+
+	nameSpace.initAnimation = function () {
+		// TweenMax can be used to set css
+		// It will even take care of browser prefixes
+		// TweenMax.set(logo, {x:100, y:50, opacity:0});
+
+		timeline = new TimelineMax( {
+			delay: 0.2,
+			onComplete: nameSpace.onAnimationComplete
+		} );
+
+		timeline.pause();
+		
+		timeline.fromTo("#copy-1", 1., {autoAlpha:0}, {autoAlpha:1, ease:Power2.easeIn})
+				.to("#copy-1", 0.5, {autoAlpha:0}, "+=2")
+				.fromTo("#copy-2", 1, {autoAlpha:0}, {autoAlpha:1, ease:Power2.easeIn})
+				.to("#copy-2", 1, {autoAlpha:0}, "+=2.25")
+				.fromTo("#copy-3, #logo", 1, {autoAlpha:0}, {autoAlpha:1, ease:Power2.easeIn})
+				.fromTo(["#copy-4", "#cta", "#arrow"], 1, {autoAlpha:0}, {autoAlpha:1, ease:Power2.easeIn},"+=1")
+
+
+
+	};
+
+	nameSpace.startAnimation = function () {
+		// Code for animation		
+		timeline.play();
+	};
+
+	nameSpace.onAnimationComplete = function () {
+		// Log duration of timeline
+		console.log( 'Animation Duration: ' + timeline.time() + 's' );
+
+		// Show a CTA or any animations outside main timeline
+
+	};
+} ) ();
